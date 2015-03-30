a8492fb73ab5894a4c73c73eaf3160ab694f24b6
Jivko Kolev <jivkokgit@gmail.com>
2015-03-30 00:57:23 -0700

    Fixup Linux setup

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -6,10 +6,10 @@ cd $HOME
 os=$(uname -s)
 if [ "$os" = "Linux" ]; then
     sudo apt-get install zsh
-    local lnflags=-sb
+    _lnflags=-sb
 elif [ "$os" = "Darwin" ]; then
     brew install zsh
-    local lnflags=-sf
+    _lnflags=-sf
 else
     echo "Unsupported OS: $os"
     return
@@ -21,10 +21,12 @@ curl -L http://install.ohmyz.sh | sh
 mkdir -p ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-ln $lnflags dotfiles/.zprofile .
-ln $lnflags dotfiles/.zshrc .
-ln $lnflags dotfiles/.zsh-theme .
+ln $_lnflags dotfiles/.zprofile .
+ln $_lnflags dotfiles/.zshrc .
+ln $_lnflags dotfiles/.zsh-theme .
 
-local _zsh=$(which zsh)
+_zsh=$(which zsh)
 [ -z "$(grep $_zsh /etc/shells)" ] && sudo -s "echo $_zsh >> /etc/shells"
 chsh -s $_zsh
+unset _lnflags _zsh
+
