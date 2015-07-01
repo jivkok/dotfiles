d456071ef0031b8bfa7cf00ec234a0f2f63d107e
Jivko Kolev <jivkokgit@gmail.com>
2015-07-01 05:17:40 -0700

    Jivko's Vim reference

--- configure_vim.sh
+++ configure_vim.sh
@@ -26,4 +26,4 @@ if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 
-vim +PluginInstall +qall
+vim +PluginInstall +"helptags ~/.vim/doc" +qall
