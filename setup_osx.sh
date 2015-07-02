e96391f840e5f031201d3e9b17f75c5ee513a47b
Jivko Kolev <jivkokgit@gmail.com>
2015-07-02 14:52:01 -0700

    Add install step for Xcode command tools

--- setup_osx.sh
+++ setup_osx.sh
@@ -6,13 +6,11 @@
 function ask_and_run ()
 {
     if [ ! -f $1 ]; then return; fi
-    read -p "$2" -n 1 -r
+    read -p "$2 " -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then return; fi
 
     source $1
-
-    tput bel
 
     return
 }
@@ -47,37 +45,47 @@ ln -sf dotfiles/.wgetrc .
 ln -sf dotfiles/osx/.duti .
 curl -o git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
+# Command-line tools
+xcode-select -p
+if [ $? != 0 ]; then
+    read -p "Would you like to install the XCode command-line tools?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        xcode-select --install
+    fi
+fi
+
 # Packages
-ask_and_run dotfiles/osx/brew.sh "Would you like to install Homebrew packages? "
+ask_and_run dotfiles/osx/brew.sh "Would you like to install Homebrew packages?"
 
 # Software
-ask_and_run dotfiles/osx/software.sh "Would you like to install additional software? "
+ask_and_run dotfiles/osx/software.sh "Would you like to install additional software?"
 
 # OSX tweaks
-ask_and_run dotfiles/osx/.osx "Would you like to set sensible OSX defaults? "
+ask_and_run dotfiles/osx/.osx "Would you like to set sensible OSX defaults?"
 
 # Python
-ask_and_run dotfiles/configure_python.sh "Would you like to configure Python and install Python packages? "
+ask_and_run dotfiles/configure_python.sh "Would you like to configure Python and install Python packages?"
 
 # Ruby
-ask_and_run dotfiles/configure_ruby.sh "Would you like to configure Ruby and install Ruby packages? "
+ask_and_run dotfiles/configure_ruby.sh "Would you like to configure Ruby and install Ruby packages?"
 
 # Configure Git
-ask_and_run dotfiles/configure_git.sh "Would you like to configure Git? "
+ask_and_run dotfiles/configure_git.sh "Would you like to configure Git?"
 
 # ZSH
-ask_and_run dotfiles/configure_zsh.sh "Would you like to configure Zsh? "
+ask_and_run dotfiles/configure_zsh.sh "Would you like to configure Zsh?"
 
 # Vim
-ask_and_run dotfiles/configure_vim.sh "Would you like to configure Vim? "
+ask_and_run dotfiles/configure_vim.sh "Would you like to configure Vim?"
 
 # SublimeText
-ask_and_run dotfiles/configure_sublimetext.sh "Would you like to configure SublimeText? "
+ask_and_run dotfiles/configure_sublimetext.sh "Would you like to configure SublimeText?"
 
 # Alfred workflows
-ask_and_run dotfiles/osx/alfred.sh "Would you like to configure Alfred workflows? "
+ask_and_run dotfiles/osx/alfred.sh "Would you like to configure Alfred workflows?"
 
 # DotNet
-ask_and_run dotfiles/osx/dotnet.sh "Would you like to configure DotNet? "
+ask_and_run dotfiles/osx/dotnet.sh "Would you like to configure DotNet?"
 
 echo "Done"
