d3cc1ca2bc3cd9edea27b3ecb5808baea36f2b4a
Jivko Kolev <jivkokgit@gmail.com>
2015-08-02 18:08:42 -0700

    Windows configuration (DOS and Powershell)

---
+++ windows/SetEnv.cmd
@@ -0,0 +1,41 @@
+@echo off
+
+set COPYCMD=/y
+
+set prompt=$_%USERNAME% @ %COMPUTERNAME% : $P$_$G$S
+
+DOSKEY cd=cd $*$Tdir
+DOSKEY ..=pushd ..
+DOSKEY ...=pushd ..\..
+DOSKEY l=dir /a /ogn /w $*
+DOSKEY ll=dir /a /ogn $*
+DOSKEY n=notepad.exe $*
+DOSKEY ds=dir /s/b $*
+DOSKEY fs=findstr /spin $1 $2
+DOSKEY b=msbuild $*
+
+rem Apps
+if exist "%ProgramW6432%\Sublime Text 3\sublime_text.exe" (
+    DOSKEY nn=start "" /B "%ProgramW6432%\Sublime Text 3\sublime_text.exe" $*
+)
+if exist "%ProgramFiles(x86)%\Git\bin\git.exe" (
+    DOSKEY g="%ProgramFiles(x86)%\Git\bin\git.exe" $*
+)
+
+rem VS
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" (
+    echo Setting VS 2015 environment ...
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" (
+    echo Setting VS 2013 environment ...
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat" (
+    echo Setting VS 2012 environment ...
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"
+)
+
+cd /d %USERPROFILE%
+
+if exist "%USERPROFILE%\profile.cmd" (
+    call "%USERPROFILE%\profile.cmd"
+)
