6eeb5049caa6deee3366fd699468234e798aefa9
Jivko Kolev <jivkokgit@gmail.com>
2015-12-27 01:38:37 -0800

    Configure NodeJS

--- setup_debian.sh
+++ setup_debian.sh
@@ -57,6 +57,8 @@ ask_and_run "$dotdir/configure_zsh.sh" "Would you like to install and configure 
 
 ask_and_run "$dotdir/configure_python.sh" "Would you like to configure Python (plus packages)?"
 
+ask_and_run "$dotdir/configure_nodejs.sh" "Would you like to configure NodeJS (plus packages)?"
+
 ask_and_run "$dotdir/configure_ruby.sh" "Would you like to configure Ruby (plus packages)?"
 
 ask_and_run "$dotdir/configure_vim.sh" "Would you like to install and configure Vim?"
