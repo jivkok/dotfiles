d73418669d30703a4e20a479813e8d07d23f51f2
Jivko Kolev <jivkokgit@gmail.com>
2016-06-05 00:48:28 -0700

    Virtual env

--- configure_python.sh
+++ configure_python.sh
@@ -9,10 +9,18 @@ if [ "$os" = "Linux" ]; then
     sudo pip install --upgrade glances # system stats
     sudo pip install --upgrade httpie # curl-like with colorized output
     sudo pip install --upgrade jsbeautifier # reformat and reindent JavaScript code. jsbeautifier.org. Use with 'js-beautify somefile.js'
+    sudo pip install --upgrade mitmproxy # http traffic interception
     sudo pip install --upgrade Pygments # syntax highlighter
 elif [ "$os" = "Darwin" ]; then
     brew install python
     pip install --upgrade pip setuptools
+
+    # Install virtual environments globally
+    # It fails to install virtualenv if PIP_REQUIRE_VIRTUALENV was true
+    export PIP_REQUIRE_VIRTUALENV=false
+    pip install virtualenv
+    pip install virtualenvwrapper
+
     # Packages
     pip install --upgrade glances # system stats
     pip install --upgrade httpie # curl-like with colorized output
