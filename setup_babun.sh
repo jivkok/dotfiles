c9dd4f272d7b51d2cb0b4435113825e6b4f57b5e
Jivko Kolev <jivkokgit@gmail.com>
2016-11-20 02:20:17 -0800

    Updated setup scripts

--- setup_babun.sh
+++ setup_babun.sh
@@ -1,51 +1,16 @@
 #!/bin/bash
 # Configure Windows environment (Babun distro for Cygwin)
 
-echo "Configuring Windows environment (Babun distro for Cygwin) ..."
+dotdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$dotdir/setupfunctions.sh"
+pull_latest_dotfiles "$dotdir"
+
+dot_trace "Configuring Windows environment (Babun distro for Cygwin) ..."
 
 # Packages
 pact update
 pact install tmux
 
-# dotfiles
-dotdir="$HOME/dotfiles"
+source "$dotdir/setupcommon.sh"
 
-if [ -d "$dotdir/.git" ]; then
-    git -C "$dotdir" pull --prune --recurse-submodules
-else
-    if [ -d "$dotdir" ]; then
-        mv "$dotdir" "${dotdir}.old"
-    fi
-    git clone --recursive https://github.com/jivkok/dotfiles.git "$dotdir"
-fi
-git -C "$dotdir" submodule update --init --recursive
-
-[ -d "$HOME/.vim" ] && mv "$HOME/.vim" "$HOME/.vim~"
-ln -sf "$dotdir/.vim" "$HOME/"
-ln -sb "$dotdir/.aliases" "$HOME/"
-ln -sb "$dotdir/.bash_profile" "$HOME/"
-ln -sb "$dotdir/.bash_prompt" "$HOME/"
-ln -sb "$dotdir/.bashrc" "$HOME/"
-ln -sb "$dotdir/.curlrc" "$HOME/"
-ln -sb "$dotdir/.editorconfig" "$HOME/"
-ln -sb "$dotdir/.exports" "$HOME/"
-ln -sb "$dotdir/.functions" "$HOME/"
-ln -sb "$dotdir/.marks.sh" "$HOME/"
-ln -sb "$dotdir/.tmux.conf" "$HOME/"
-ln -sb "$dotdir/.vim/.vimrc" "$HOME/"
-ln -sb "$dotdir/.wgetrc" "$HOME/"
-curl -o "$HOME/git-prompt.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-
-# Git
-[ -f "$dotdir/configure_git.sh" ] && source "$dotdir/configure_git.sh"
-
-mkdir -p "$HOME/.oh-my-zsh/custom/plugins"
-[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ] && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-ln -sb "$dotdir/.zprofile" "$HOME/"
-ln -sb "$dotdir/.zshrc" "$HOME/"
-ln -sb "$dotdir/.zsh-theme" "$HOME/"
-
-source "$HOME/.zprofile"
-source "$HOME/.zshrc"
-
-echo "Configuring Windows environment (Babun distro for Cygwin) done."
+dot_trace "Configuring Windows environment (Babun distro for Cygwin) done."
--- setup_debian.sh
+++ setup_debian.sh
@@ -1,75 +1,15 @@
 #!/bin/bash
 # Configure Linux environment (Debian-style)
 
-echo "Configuring Linux environment (Debian-style) ..."
+dotdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$dotdir/setupfunctions.sh"
+pull_latest_dotfiles "$dotdir"
 
-# $1 - file, $2 - message
-function ask_and_run ()
-{
-    if [ ! -f "$1" ]; then return; fi
-    shh=$(ps -p $$ -oargs=)
-    if [ "-zsh" = "$shh" ]; then
-        read -r "REPLY?$2 "
-    else
-        read -p "$2 " -n 1 -r
-    fi
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then return; fi
+dot_trace "Configuring Linux environment (Debian-style) ..."
 
-    source "$1"
+confirm_and_run "$dotdir/linux/packages.sh" "system packages"
+confirm_and_run "$dotdir/linux/software.sh" "GUI packages"
 
-    return
-}
+source "$dotdir/setupcommon.sh"
 
-# dotfiles
-dotdir="$HOME/dotfiles"
-
-if [ -d "$dotdir/.git" ]; then
-    git -C "$dotdir" pull --prune --recurse-submodules
-    git -C "$dotdir" submodule update --init --recursive
-else
-    if [ -d "$dotdir" ]; then
-        mv "$dotdir" "${dotdir}.old"
-    fi
-    git clone --recursive https://github.com/jivkok/dotfiles.git "$dotdir"
-fi
-
-mkdir -p "$HOME/.config"
-
-ln -sf "$dotdir/.vim" "$HOME/"
-ln -sf "$dotdir/.vim" "$HOME/.config/nvim"
-ln -sb "$dotdir/.aliases" "$HOME/"
-ln -sb "$dotdir/.bash_profile" "$HOME/"
-ln -sb "$dotdir/.bash_prompt" "$HOME/"
-ln -sb "$dotdir/.bashrc" "$HOME/"
-ln -sb "$dotdir/.ctags" "$HOME/"
-ln -sb "$dotdir/.curlrc" "$HOME/"
-ln -sb "$dotdir/.editorconfig" "$HOME/"
-ln -sb "$dotdir/.exports" "$HOME/"
-ln -sb "$dotdir/.functions" "$HOME/"
-ln -sb "$dotdir/.marks.sh" "$HOME/"
-ln -sb "$dotdir/.tmux.conf" "$HOME/"
-ln -sb "$dotdir/.vim/.vimrc" "$HOME/"
-ln -sb "$dotdir/.wgetrc" "$HOME/"
-
-curl -o "$HOME/git-prompt.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-
-ask_and_run "$dotdir/linux/packages.sh" "Would you like to install system packages?"
-
-ask_and_run "$dotdir/linux/software.sh" "Would you like to install GUI packages?"
-
-ask_and_run "$dotdir/configure_git.sh" "Would you like to configure Git?"
-
-ask_and_run "$dotdir/configure_zsh.sh" "Would you like to install and configure ZSH?"
-
-ask_and_run "$dotdir/configure_python.sh" "Would you like to configure Python (plus packages)?"
-
-ask_and_run "$dotdir/configure_nodejs.sh" "Would you like to configure NodeJS (plus packages)?"
-
-ask_and_run "$dotdir/configure_ruby.sh" "Would you like to configure Ruby (plus packages)?"
-
-ask_and_run "$dotdir/configure_vim.sh" "Would you like to install and configure Vim?"
-
-ask_and_run "$dotdir/configure_sublimetext.sh" "Would you like to configure SublimeText?"
-
-echo "Configuring Linux environment (Debian-style) done."
+dot_trace "Configuring Linux environment (Debian-style) done."
