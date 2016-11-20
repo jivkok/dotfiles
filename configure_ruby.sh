0bfb050f2e4532a45cf62566f6f733ed3775b161
Jivko Kolev <jivkokgit@gmail.com>
2016-11-20 02:19:56 -0800

    Install Node/Python/Ruby packages in the user folder

--- configure_ruby.sh
+++ configure_ruby.sh
@@ -3,27 +3,29 @@
 
 os=$(uname -s)
 if [ "$os" = "Linux" ]; then
-    # ## Using RVM:
+    # Using RVM: https://gorails.com/setup/ubuntu/14.04
+    # sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
     # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    # curl -L https://get.rvm.io | bash -s stable
+    # curl -sSL https://get.rvm.io | bash -s stable
     # source ~/.rvm/scripts/rvm
-    # rvm requirements
-    # touch $HOME/profile_extra
-    # echo export PATH="\$PATH:\$HOME/.rvm/bin">>$HOME/.profile_extra
-    # echo [[ -s "\$HOME/.rvm/scripts/rvm" ]] && source "\$HOME/.rvm/scripts/rvm">>$HOME/.profile_extra
-    sudo apt-get install rubygems-integration
+    # rvm install 2.3.1
+    # rvm use 2.3.1 --default
+    # ruby -v
+    sudo apt-get install -y rubygems-integration
 elif [ "$os" = "Darwin" ]; then
     ! brew ls --versions ruby-build >/dev/null 2>&1 && brew install ruby-build
     ! brew ls --versions rbenv >/dev/null 2>&1 && brew install rbenv
 
     # Packages
-    sudo gem install cocoapods
+    gem install --user-install cocoapods
 else
     echo "Unsupported OS: $os"
     return
 fi
 
-sudo gem install compass
-sudo gem install tmuxinator
+# Packages
+gem install --user-install compass
+gem install --user-install tmuxinator
 
 sudo gem update --system
+gem update -y
