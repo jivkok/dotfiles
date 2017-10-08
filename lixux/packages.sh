661f2c9bdb1ce6d36f05093acc1d7fa647967176
Jivko Kolev <jivkokgit@gmail.com>
2017-10-08 14:15:42 -0700

    Apt changes

--- lixux/packages.sh
+++ lixux/packages.sh
@@ -21,6 +21,7 @@ sudo apt-get install -y ngrep # grep for network traffic
 sudo apt-get install -y mosh # Mobile shell that supports roaming and intelligent local echo
 sudo apt-get install -y python-pip # alternative Python package installer
 sudo apt-get install -y python-pygments # syntax highlighting package written in Python
+sudo apt-get install -y ranger # caca-utils highlight atool w3m poppler-utils mediainfo # File manager with an ncurses frontend written in Python
 sudo apt-get install -y rlwrap # readline feature command line wrapper
 sudo apt-get install -y silversearcher-ag # very fast grep-like program, alternative to ack-grep
 sudo apt-get install -y shellcheck # lint tool for shell scripts
@@ -28,6 +29,7 @@ sudo apt-get install -y tmux # terminal multiplexer
 sudo apt-get install -y tree # displays an indented directory tree, in color
 sudo apt-get install -y vim # Vi IMproved - enhanced vi editor
 sudo apt-get install -y wget # retrieves files from the web
+sudo apt-get install -y xsel # command line tool to access X clipboard and selection buffers
 
 sudo apt-get install -y libxml2-dev # Development files for the GNOME XML library
 sudo apt-get install -y libxslt1-dev # XSLT 1.0 processing library - development kit
@@ -40,12 +42,15 @@ else
 fi
 [ -f "$HOME/.fzf/install" ] && "$HOME/.fzf/install" --key-bindings --completion --no-update-rc
 
-# Security-related
+# Diagnostics
+sudo apt-get install -y atop # Monitor for system resources and process activity
 sudo apt-get install -y htop # interactive processes viewer
 sudo apt-get install -y iftop # displays bandwidth usage information on an network interface
+sudo apt-get install -y iotop # simple top-like I/O monitor
 sudo apt-get install -y lsof # Utility to list open files
 sudo apt-get install -y ltrace # Tracks runtime library calls in dynamically linked programs
 sudo apt-get install -y nethogs # Net top tool grouping bandwidth per process
+sudo apt-get install -y ntop # display network usage in web browser
 sudo apt-get install -y secure-delete # tools to wipe files, free disk space, swap and memory
 sudo apt-get install -y strace # System call tracer
 
