3fcb6c865f1e8673321bc3bd1ca1bfc8fe3d5bcf
Jivko Kolev <jivkokgit@gmail.com>
2017-12-11 23:25:56 -0800

    Descriptions for node modules

--- configure_nodejs.sh
+++ configure_nodejs.sh
@@ -20,21 +20,22 @@ else
 fi
 
 # Packages
-npm install -g bower
-npm install -g coffee-script
-npm install -g eslint
-npm install -g express
-npm install -g express-generator
-npm install -g generator-angular
-npm install -g generator-webapp
-npm install -g grunt-cli
-npm install -g gulp
-npm install -g http-server
-npm install -g jshint
-npm install -g less
-npm install -g n
-npm install -g nodemon
-npm install -g typescript
-npm install -g yo
+npm install -g bower # The browser package manager
+npm install -g coffee-script # Unfancy JavaScript
+npm install -g diff2html-cli # Fast Diff to colorized HTML
+npm install -g eslint # An AST-based pattern checker for JavaScript
+npm install -g express # Fast, unopinionated, minimalist web framework
+npm install -g express-generator # Express application generator
+npm install -g generator-angular # Yeoman generator for AngularJS
+npm install -g generator-webapp # Scaffold out a front-end web app
+npm install -g grunt-cli # The grunt command line interface
+npm install -g gulp # The streaming build system
+npm install -g http-server # A simple zero-configuration command-line http server
+npm install -g jshint # Static analysis tool for JavaScript
+npm install -g less # Leaner CSS
+npm install -g n # Interactively Manage All Your Node Versions
+npm install -g nodemon # Simple monitor script for use during development of a node.js app
+npm install -g typescript # TypeScript is a language for application scale JavaScript development
+npm install -g yo # CLI tool for running Yeoman generators
 
 npm cache verify
