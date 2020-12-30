63e931bf9272724e92d810d7e36ac54d0545b9e8
Jivko Kolev <jivkokgit@gmail.com>
2020-12-30 13:15:12 -0800

    Updating BitBarPlugins

--- osx/BitBarPlugins/CalendarLite.1m.sh
+++ osx/BitBarPlugins/CalendarLite.1m.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # <bitbar.title>Clock with calendar</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
@@ -9,29 +9,34 @@
 # <bitbar.dependencies>bash</bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/WeibingChen17/</bitbar.abouturl>
 
-date "+%l:%M %p"
-echo "---"
+function print-month() {
+  local month_offset="$1"
+  local month=$(date -v${month_offset}m +%m)
+  local year=$(date -v${month_offset}m +%Y)
+  local month_name=$(date -jf %Y-%m-%d "$year"-"$month"-01 '+%b')
+
+  echo "---"
+  echo "$month_name $year|trim=false font=$font"
+  cal -d "$year"-"$month" |awk 'NF'|sed 's/ *$//'| while IFS= read -r i; do echo "--$i|trim=false font=$font"; done
+}
+
 font="Monaco"
-color="red"
+color="blue"
 
-#Uncomment the below line and comment out all other following lines to trigger the three-month mode
-#cal - 3 |awk 'NF'|sed 's/ $//' |while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/'  ; done
+echo "$(date "+%l:%M %p")|size=12"
 
-#Comment out these lines to remove "last month"
-last_month=$(date -v-1m +%m)
-last_year=$(date -v-1m +%Y)
-last_month_name=$(date -jf %Y-%m-%d "$last_year"-"$last_month"-01 '+%b')
-echo "Prev: $last_month_name $last_year|trim=false font=$font"
-cal -d "$last_year"-"$last_month" |awk 'NF'|sed 's/ *$//'| while IFS= read -r i; do echo "--$i|trim=false font=$font"; done 
+# For a flat 3-month view: uncomment the line below and comment out all the lines below
+#echo "---"
+#cal -3 |awk 'NF'|sed 's/ $//' |while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/'  ; done
+
+print-month "-3"
+print-month "-2"
+print-month "-1"
+
 echo "---"
+#cal |awk 'NF'|while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/' |sed 's/ *$//'; done
+cal |awk 'NF'|while IFS= read -r i; do echo " $i"|perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/' |sed 's/ *$//' |sed "s/$/|trim=false font=$font color=$color/"; done
 
-#cal |awk 'NF'|while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/' |sed 's/ *$//'; done 
-cal |awk 'NF'|while IFS= read -r i; do echo " $i"|perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/' |sed 's/ *$//' |sed "s/$/|trim=false font=$font color=$color/"; done 
-
-#Comment out these lines to remove "next month"
-echo "---"
-next_month=$(date -v+1m +%m)
-next_year=$(date -v+1m +%Y)
-next_month_name=$(date -jf %Y-%m-%d "$next_year"-"$next_month"-01 '+%b')
-echo "Next: $next_month_name $next_year|trim=false font=$font"
-cal -d "$next_year"-"$next_month" | awk 'NF'|sed 's/ *$//' | while IFS= read -r i; do echo "--$i|trim=false font=$font";done
+print-month "+1"
+print-month "+2"
+print-month "+3"
