ca2bb69033cc90b808b9abd67132874eb910df2f
JK <jivkokgit@gmail.com>
2023-11-12 14:49:40 -0800

    Docker utils cleanup

--- sh/setenv.sh
+++ sh/setenv.sh
@@ -1,7 +1,3 @@
-_has() {
-  return $(command -v "$1" >/dev/null 2>&1)
-}
-
 source "$dotdir/sh/path.sh"
 source "$dotdir/sh/system.sh"
 source "$dotdir/sh/ls.sh"
@@ -11,5 +7,5 @@ source "$dotdir/sh/utils.sh"
 source "$dotdir/sh/web.sh"
 source "$dotdir/sh/marks.sh"
 source "$dotdir/git/git.sh"
-_has docker && source "$dotdir/docker/docker.sh"
+source "$dotdir/docker/docker.sh"
 [ "$(uname -s)" = "Darwin" ] && source "$dotdir/osx/setenv.sh"
